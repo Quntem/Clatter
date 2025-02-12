abe4d8bcf99aec0c5d9e2bbb2ba0de281ac1bf7c
Oscar May <oscar@quntem.co.uk>
2025-02-12 20:22:49 +0000

    maybe fix date sent but idk i cant test 🤷‍♀️

--- app/server.js
+++ app/server.js
@@ -156,6 +156,8 @@ io.on("connection", (socket) => {
   }) 
   socket.on("clatter.channel.message.send", async (args) => {
     var argjson = JSON.parse(args)
+
+    args.DateCreated = new Date().toISOString()
     
     socket.emit("clatter.channel.message.send.response", "Sent Message")
     socket.to(argjson.room).emit("clatter.channel.message.recieve", args)
