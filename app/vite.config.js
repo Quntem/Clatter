a7b902362fc291a8f5846a9fc1b0d1cc49130f41
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2025-03-19 18:42:03 +0000

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     host: '0.0.0.0', // Ensures Vite is accessible from the container
     allowedHosts: [
       "clatter.quntem.co.uk",
-      "clatterpreview.quntem.co.uk"
+      "clatterpreview.quntem.co.uk",
+      "clatter.work"
     ],
     port: 3000,      // Matches the exposed port in docker-compose
   },
