dcb940ee27c0b59fb1075953e090ee476b48a4e5
Oscar May <oscar@quntem.co.uk>
2025-06-04 14:57:26 +0100

    documents now work in ease client

--- app/documenteditor.js
+++ app/documenteditor.js
@@ -21,5 +21,35 @@ window.LaunchDocEditor = function() {
             },
             header: Header,
         },
+        onChange: async function() {
+            window.editor.save().then((outputData) => {
+                var req = fetch("/api/document/" + currentdocumentid + "/content", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        accept: "application/json",
+                    },
+                    body: JSON.stringify({
+                        content: outputData
+                    })
+                })
+                req.then(res => {
+                    console.log(res)
+                })
+              }).catch((error) => {
+                console.log('Saving failed: ', error)
+              });
+        },
+        onReady: async function() {
+            var req = await fetch("/api/document/" + currentdocumentid + "/content")
+            var content = await req.json()
+            window.editor.render(content.content)
+            console.log(content)
+            if(window.location.href.includes("client")) {
+                $("#mainarea-header-title").text(content.name)
+            } else {
+
+            }
+        },
     });
 }
